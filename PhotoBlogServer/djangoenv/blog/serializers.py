e102bf4e64a08b0c6c15f6169468e88f5a5e08d7
sodlfmag <sodlfmag@khu.ac.kr>
2025-11-17 23:31:14 +0900

    이미지 절대경로로 반환하도록 수정

--- PhotoBlogServer/djangoenv/blog/serializers.py
+++ PhotoBlogServer/djangoenv/blog/serializers.py
@@ -4,7 +4,17 @@ from django.contrib.auth.models import User
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = ('author', 'title', 'text', 'created_date', 'published_date', 'image')
+    
+    def get_image(self, obj):
+        if obj.image:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.image.url)
+            # request가 없는 경우 (예: 관리자 페이지)
+            return obj.image.url if obj.image.url.startswith('http') else None
+        return None
