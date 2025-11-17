250dc4cbf7c45cdd2fe0fa7bc0e6953abb6df7a7
sodlfmag <sodlfmag@khu.ac.kr>
2025-11-18 00:02:31 +0900

    이미지 필드 오류 수정

--- PhotoBlogServer/djangoenv/blog/serializers.py
+++ PhotoBlogServer/djangoenv/blog/serializers.py
@@ -5,16 +5,20 @@ from django.contrib.auth.models import User
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     image = serializers.ImageField(required=False, allow_null=True)
-    image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ('author', 'title', 'text', 'created_date', 'published_date', 'image', 'image_url')
+        fields = ('author', 'title', 'text', 'created_date', 'published_date', 'image')
 
-    def get_image_url(self, obj):
-        if obj.image:
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        image = instance.image
+        if image:
             request = self.context.get('request')
+            url = image.url
             if request:
-                return request.build_absolute_uri(obj.image.url)
-            return obj.image.url
-        return None
+                url = request.build_absolute_uri(url)
+            data['image'] = url
+        else:
+            data['image'] = None
+        return data
