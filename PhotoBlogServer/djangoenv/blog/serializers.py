9a69a7a075fd33bf2f051ed87b28218a513a5014
sodlfmag <sodlfmag@khu.ac.kr>
2025-11-17 23:49:23 +0900

    Meta 필드 오류 수정

--- PhotoBlogServer/djangoenv/blog/serializers.py
+++ PhotoBlogServer/djangoenv/blog/serializers.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import User
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    image = serializers.SerializerMethodField()
+    image = serializers.ImageField(required=False, allow_null=True)
+    image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ('author', 'title', 'text', 'created_date', 'published_date', 'image')
-    
-    def get_image(self, obj):
+        fields = ('author', 'title', 'text', 'created_date', 'published_date', 'image', 'image_url')
+
+    def get_image_url(self, obj):
         if obj.image:
             request = self.context.get('request')
             if request:
                 return request.build_absolute_uri(obj.image.url)
-            # request가 없는 경우 (예: 관리자 페이지)
-            return obj.image.url if obj.image.url.startswith('http') else None
+            return obj.image.url
         return None
